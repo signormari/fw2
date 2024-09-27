ad4aed3d3db632116e9ca01c88e0e1cb11d2d53a
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-09-27 14:23:07 +0000

    Aula 27-09_2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,9 @@
-function Index(){
-    return <h1>Olá Mundo</h1>
+import Footer from "./component/footer";
+import Navbar from "./component/navbar";
+export default function Index() {
+    return <>
+        <Navbar />
+        <h1>Olá Mundo</h1>
+        <Footer />
+    </>;
 }
-export default Index;
