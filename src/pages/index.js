12cac872cc3e96f437795c4cafae7c4a9d53cced
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-09-30 15:45:44 +0000

    Aula 30-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
 import Footer from "./component/footer";
 import Navbar from "./component/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default function Index() {
     return <>
         <Navbar />
-        <h1>Olá Mundo</h1>
+        <Container>
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
         <Footer />
     </>;
 }
