f6d60015d700d9c8b66eeadc8f61b227c0e93ebc
signormari <120032546+signormari@users.noreply.github.com>
2024-10-28 15:29:55 +0000

    aula 28/10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,19 +1,31 @@
 import { Container } from "react-bootstrap";
 import Menu from "./component/navbar";
 import Footer from "./component/footer";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Promessa(){
     const[prometido, setPrometido] = useState("");
-    setTimeout(()=>{
-        setPrometido("é uma divida");
-        document.title="Promessa é divida";
-    }, 2000);
+    useEffect(() => {
+        setTimeout(async ()=>{
+            document.title="Promessa é divida";
+            const altera = await mudaTexto("ola texto novo");
+            setPrometido(altera);
+        }, 2000);
+    });
+    
+
     return <>
     <Menu />
     <Container>
-        Promessa {prometido=="" ? <img src="./loading.gif" width={30} height={30}></img> : prometido}
+        Promessa {prometido === "" ? <img src="./loading.gif" width={30} height={30}></img> : prometido}
     </Container>
     <Footer />
     </>
 }
+function mudaTexto(texto){
+    return new Promise ((resolva) =>
+    setTimeout(() => {
+        resolva(texto)
+    }, 1000)
+)
+};
